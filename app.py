a94f9a20816a991d0e834ff909d2bbf9ca32228c
Roshu2 <kyumin1020@gmail.com>
2022-05-19 15:23:16 +0900

    1. 회원가입 api 기본틀 2. 로그인 api 기본틀 3. 연결완료

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from functools import wraps
 import hashlib
 import json
+from re import S
 from bson import ObjectId
 import jwt
 from datetime import datetime, timedelta
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
 
@@ -12,22 +14,25 @@ SECRET_KEY = 'recycle'
 app = Flask(__name__)
 cors = CORS(app, resources={r'*': {'origins': '*'}})
 client = MongoClient('localhost', 27017)
-db = client.turtlegram
+db = client.tencycle
 
-@app.route("/")
-def hello_word():
-    return jsonify({'msg':'success'})
+@app.route('/')
+def home():
+    return jsonify({'msg' : 'success'})
 
 @app.route("/signup", methods=["POST"])
 def sign_up():
     
     data = json.loads(request.data)
+    print(data)
     
     password_hash = hashlib.sha256(data['password'].encode('utf-8')).hexdigest()
     
     doc = {
-        'email' : data.get('email'),
-        'password' : password_hash
+        'username' : data.get('username'),
+        'userid' : data.get('userid'),
+        'password' : password_hash,
+        'userpoint' : '0'
     }
     
     db.users.insert_one(doc)
@@ -38,15 +43,16 @@ def sign_up():
 @app.route("/login", methods=["POST"])
 def login():
     data = json.loads(request.data)
+    print(data)
     
-    email = data.get("email")
+    userid = data.get("userid")
     password = data.get("password")
     
      # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
     hashed_pw = hashlib.sha256(password.encode('utf-8')).hexdigest()
     
     result = db.users.find_one({
-        'email': email, 
+        'userid': userid, 
         'password': hashed_pw
     })
     
@@ -63,31 +69,14 @@ def login():
         return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-    
-    
-    
-@app.route("/getuserinfo", methods=["GET"])
-def get_user_info():
-    token = request.headers.get("Authorization")
-    
-    
-    if not token:
-        return jsonify({"message": "no token"}), 402
-    
-    user = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-    print(user)
-    result = db.users.find_one({
-        '_id': ObjectId(user["id"])
-    })
-    
-    print(result)
-    
-    
-    
-    return jsonify({"msg": "success", "email": result["email"]})
 
 
-    
+
+
+
+
+
+
 
 if __name__ =='__main__':
     app.run('0.0.0.0', port=5000, debug=True)
