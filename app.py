977915d6cf6e3a06efeac728f88e493e98f88e3c
원송희 <songheew1020@gmail.com>
2022-05-23 15:43:29 +0900

    사진 업로드 api 작성

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from functools import wraps
 import hashlib
 import json
 from re import S
-from bson import ObjectId
+#from bson import ObjectId
 import jwt
 from datetime import datetime, timedelta
 from flask import Flask, abort, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
+import certifi
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.1idhr.mongodb.net/cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+db = client.dbsparta
 
 SECRET_KEY = 'recycle'
 
@@ -71,12 +75,40 @@ def login():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
+@app.route("/upload", methods=["POST"])
+def image_predict():
+    img=request.files['file'] # 이미지 파일
+    user_id=request.form['id_give'] # 사용자 ID
+    now_time=datetime.now() # 현재 시각
+    ext_tail=img_name.split(".")[-1] # 확장자 추출
+    file_name=f"{current_time.strftime('%Y%m%d%H%M%S')}.{ext_tail}" # '날짜.확장자' 를 파일명으로 지정
+    save_location=f"/static/image/{file_name}" # 저장할 장소
+    img.save(save_location) # 이미지 저장
+
+    # 예측
+    pred=load_model.predict(save_location)
+
+    # DB로 결과와 함께 전달
+    doc={
+        'user_id': user_id,
+        'image': img,
+        'category': pred,
+        'date': now_time
+    }
+    db.recycles.insert_one(doc)
+
+    return jsonify({'msg': '예측 완료!'})
 
 
+@app.route("/main", methods=["GET"])
+def get_image():
+    user_id=request.form['id_give'] # 사용자 ID
+    result=db.recycles.find_one({'user_id': user_id}) # 가장 최근꺼 결과 가져오기
 
+    return jsonify({'img': img})
 
 
 
 
 if __name__ =='__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5500, debug=True)
